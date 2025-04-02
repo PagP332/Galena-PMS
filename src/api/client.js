b92e9d3289401b22cf4dc416a67d243727704574
Paun Wander <tsme0613@gmail.com>
2025-04-02 13:15:10 +0800

    Fixes for preview deploy

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,5 @@
 import { createClient } from "@supabase/supabase-js"
 // import config from "./key"
-import dotenv from "dotenv"
-dotenv.config()
 
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_ANON_KEY
