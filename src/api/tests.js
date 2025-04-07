3d2725ab42745340f54f2f669ae3a426c75a3e43
Paun Wander <tsme0613@gmail.com>
2025-04-08 01:22:07 +0800

    Added global font style

--- src/api/tests.js
+++ src/api/tests.js
@@ -1,12 +1,12 @@
 import { supabase } from "./client"
 
 export const testFetch = async () => {
-  const { data, error } = await supabase.from("test_data").select("foo").eq("id", 2)
+  const { data } = await supabase.from("test_data").select("foo").eq("id", 2)
   console.log(data[0])
   return data[0].foo
 }
 
 export const testUpdate = async (newData) => {
-  const { data, error } = await supabase.from("test_data").update({ foo: newData }).eq("id", 2).select()
+  const { data } = await supabase.from("test_data").update({ foo: newData }).eq("id", 2).select()
   console.log(data)
 }
